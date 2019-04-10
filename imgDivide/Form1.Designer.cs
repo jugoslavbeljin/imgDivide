569628903264ba3b16d8bb5dcd8d61e63e3ca968
Jugoslav Beljin <jugab@ptt.rs>
2019-04-10 15:04:07 +0200

    verzija 01

--- imgDivide/Form1.Designer.cs
+++ imgDivide/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace imgDivide
 {
-    partial class Form1
+    partial class frm_divide
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.txt_source = new System.Windows.Forms.TextBox();
+            this.txt_destination = new System.Windows.Forms.TextBox();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.txt_interval = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_doit = new System.Windows.Forms.Button();
+            this.fBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(422, 12);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.TabIndex = 0;
+            this.btnSource.Text = "Source";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // txt_source
+            // 
+            this.txt_source.Location = new System.Drawing.Point(10, 12);
+            this.txt_source.Name = "txt_source";
+            this.txt_source.Size = new System.Drawing.Size(388, 20);
+            this.txt_source.TabIndex = 1;
+            // 
+            // txt_destination
+            // 
+            this.txt_destination.Location = new System.Drawing.Point(10, 48);
+            this.txt_destination.Name = "txt_destination";
+            this.txt_destination.Size = new System.Drawing.Size(388, 20);
+            this.txt_destination.TabIndex = 2;
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(422, 44);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(75, 23);
+            this.btnDestination.TabIndex = 3;
+            this.btnDestination.Text = "Destination";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // txt_interval
+            // 
+            this.txt_interval.Location = new System.Drawing.Point(298, 87);
+            this.txt_interval.Name = "txt_interval";
+            this.txt_interval.Size = new System.Drawing.Size(100, 20);
+            this.txt_interval.TabIndex = 4;
+            this.txt_interval.Text = "60";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Session interval";
+            // 
+            // btn_doit
+            // 
+            this.btn_doit.Location = new System.Drawing.Point(422, 171);
+            this.btn_doit.Name = "btn_doit";
+            this.btn_doit.Size = new System.Drawing.Size(75, 23);
+            this.btn_doit.TabIndex = 6;
+            this.btn_doit.Text = "Do it";
+            this.btn_doit.UseVisualStyleBackColor = true;
+            this.btn_doit.Click += new System.EventHandler(this.btn_doit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Prefix";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(298, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "ROOM1_MMDD";
+            // 
+            // frm_divide
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(523, 206);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_doit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_interval);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.txt_destination);
+            this.Controls.Add(this.txt_source);
+            this.Controls.Add(this.btnSource);
+            this.Name = "frm_divide";
+            this.Text = "Divide images to folders";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.TextBox txt_source;
+        private System.Windows.Forms.TextBox txt_destination;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.TextBox txt_interval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_doit;
+        private System.Windows.Forms.FolderBrowserDialog fBD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- imgDivide/Form1.cs
+++ imgDivide/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,70 @@ using System.Windows.Forms;
 
 namespace imgDivide
 {
-    public partial class Form1 : Form
+    public partial class frm_divide : Form
     {
-        public Form1()
+        public frm_divide()
         {
             InitializeComponent();
+        }
+
+        private void btn_doit_Click(object sender, EventArgs e)
+        {
+            //spisak file-ova u folderu - super bi bilo ako moze da se sortira po datumu
+
+            String path_ = @"c:\";
+            DirectoryInfo di = new DirectoryInfo(txt_source.Text);
+            FileInfo[] fiArray = di.GetFiles();
+
+            Array.Sort(fiArray, (x, y) => StringComparer.OrdinalIgnoreCase.Compare(x.CreationTime, y.CreationTime));
+
+            DateTime lastDT_=fiArray[0].CreationTime;
+
+            foreach (FileInfo fi in fiArray)
+            {
+                //sada mozes dalje da ispitujes
+                TimeSpan ts_ = fi.CreationTime - lastDT_;
+                if (ts_.Minutes<Int32.Parse(txt_interval.Text))
+                {
+                    //sada ga kopira u nesto a ako ne 
+
+                } else
+                {
+                    //znaci da je potrebno resetovati sve na pocetak
+                    //prvo idemo na lastDT_
+                    lastDT_ = fi.CreationTime;
+                    //pravimo novi folder
+
+                    //kopiramo file u njega
+                    String destination = txt_destination.Text;
+                    fi.CopyTo(destination + "\\" + fi.Name);
+                }
+            }
+
+
+
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            fBD.ShowNewFolderButton = false;
+            fBD.SelectedPath = "";
+            fBD.ShowDialog();
+            if (fBD.SelectedPath != null)
+            {
+                txt_source.Text = fBD.SelectedPath;
+            }
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            fBD.ShowNewFolderButton = true;
+            fBD.SelectedPath = "";
+            fBD.ShowDialog();
+            if (fBD.SelectedPath != null)
+            {
+                txt_destination.Text = fBD.SelectedPath;
+            }
         }
     }
 }
